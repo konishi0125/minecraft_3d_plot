b3554907c1686dcb7aa1b0dbac0a9cf4b52c6ac6
konishi0125 <yumemita@gmail.com>
2021-12-15 20:34:00 +0900

    input size

--- make_3d.py
+++ make_3d.py
@@ -4,11 +4,10 @@ import mcpi.minecraft as minecraft
 import mcpi.block as block
 import obj2dot
 
-
 vertices, uvs, normals, faceVertIDs, uvIDs, normalIDs, vertexColors = \
     obj2dot.loadOBJ(sys.argv[1])
 P = np.array(vertices)
-P = obj2dot.obj2dot(P, 256)
+P = obj2dot.obj2dot(P, sys.argv[2])
 
 mc = minecraft.Minecraft()
 playerPos = mc.player.getPos()
